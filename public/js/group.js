9c11de0cbf3f5fbb1e66bb34989e371232787a94
lazhar233 <lazhhar3@gmail.com>
2024-11-03 22:01:12 +0100

    seconde15

--- public/js/group.js
+++ public/js/group.js
@@ -2,10 +2,7 @@
 
 const bdy = document.getElementById("bdy");
 
-for( let i = 1; i <5 ; i++ ){
 
-
-}
 
 
  for (let i = 1; i <= 4; i++){
@@ -65,42 +62,88 @@ function care(i) {
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
 
-// تعريف المصفوفات
-const namess = ["lazhar", "imad", "yacin", "mohamed", "ilyas", "ayman", "lakhdar", "salim", "mokhtar", "djamal"];
-const images = ["/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png"];
+// // تعريف المصفوفات
+// const namess = ["lazhar", "imad", "yacin", "mohamed", "ilyas", "ayman", "lakhdar", "salim", "mokhtar", "djamal"];
+// const images = ["/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png", "/backgrnd/1.png"];
 
 
-function charg(nom, image) {
-    // البحث عن العنصر "bdy"
-    const bdy = document.getElementById("bdy");
+// function charg(nom, image) {
+//     // البحث عن العنصر "bdy"
+//     const bdy = document.getElementById("bdy");
    
 
-    // إنشاء العناصر وإعدادها
-    const jma3a = document.createElement('div');
-    const h4 = document.createElement('h4');
-    const h3 = document.createElement('h3');
-    const img = document.createElement('img');
+//     // إنشاء العناصر وإعدادها
+//     const jma3a = document.createElement('div');
+//     const h4 = document.createElement('h4');
+//     const h3 = document.createElement('h3');
+//     const img = document.createElement('img');
 
-    jma3a.id = 'jma3a'; 
+//     jma3a.id = 'jma3a'; 
     
-    h4.innerHTML = nom;       // إضافة الاسم من المصفوفة
-    h3.innerHTML = "PAINTER"; // النص الثابت "PAINTER"
-    img.src = image;          // إضافة الصورة من المصفوفة
-    img.alt = "error";
+//     h4.innerHTML = nom;       // إضافة الاسم من المصفوفة
+//     h3.innerHTML = "PAINTER"; // النص الثابت "PAINTER"
+//     img.src = image;          // إضافة الصورة من المصفوفة
+//     img.alt = "error";
 
-    jma3a.appendChild(img);
-    jma3a.appendChild(h4);
-    jma3a.appendChild(h3);
+//     jma3a.appendChild(img);
+//     jma3a.appendChild(h4);
+//     jma3a.appendChild(h3);
 
-    bdy.appendChild(jma3a);
-}
+//     bdy.appendChild(jma3a);
+// }
 
-// التأكد من أن العنصر "bdy" موجود في HTML قبل استدعاء الدالة
-for (let i = 0; i < 10; i++) {
-    charg(namess[i], images[i]);
-}
+// // التأكد من أن العنصر "bdy" موجود في HTML قبل استدعاء الدالة
+// for (let i = 0; i < 10; i++) {
+//     charg(namess[i], images[i]);
+// }
 
-})
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 1; i <= 4; i++) {
+//     care(i);
+// }
+// function care(i) {
+//     var car = document.createElement('div');
+//     var btn = document.createElement('button');
+//     var hsx = document.createElement('h6');
+//     var gren = document.createElement('div'); 
+
+
+//     btn.innerHTML = 'Click';  
+//     // const ray=[1,2,3,4]
+//     const ray = [{ num: "دهان" }, { num: "حلاف" }, { num: "بناء" }, { num: "سباك" }];
+//     hsx.innerHTML = `${ray[i-1].num}`;
+
+//     car.id = 'car'; 
+//     car.style.backgroundImage= ` url("/backgrnd/${i}.png")`;
+    
+//     gren.id = 'gren'; 
+
+//     gren.appendChild(btn);
+//     gren.appendChild(hsx);
+//     car.appendChild(gren);
+//     bdy.appendChild(car);
+// }
+
+
+
--- public/js/home.js
+++ public/js/home.js
@@ -45,30 +45,9 @@ bdy.addEventListener('click', function() {
 
 
 for (let i = 1; i <= 4; i++) {
-    // const element = array[i];
     care(i);
 }
-       
-// function care(i) {
-//     var car = document.createElement('div');
-//     var btn = document.createElement('button');
-//     var hsx = document.createElement('h6');
 
-//     btn.innerHTML = 'Click';  
-//     hsx.innerHTML = 'If using margin, gap between';
-
-//     car.id = 'car'; 
-//     car.style.backgroundImage= ` url("/backgrnd/${i}.png")`;
-//     var gren = document.createElement('div'); 
-//     gren.id = 'gren'; 
-
-//     gren.appendChild(btn);
-//     gren.appendChild(hsx);
-
-//     car.appendChild(gren);
-    
-//     bdy.appendChild(car);
-// }
 
 
 
@@ -81,8 +60,8 @@ function care(i) {
 
     btn.innerHTML = 'Click';  
     // const ray=[1,2,3,4]
-    const ray = [{ num: "1" }, { num: "2" }, { num: "3" }, { num: "4" }];
-    hsx.innerHTML = ray[i-1];
+    const ray = [{ num: "دهان" }, { num: "حلاف" }, { num: "بناء" }, { num: "سباك" }];
+    hsx.innerHTML = `${ray[i-1].num}`;
 
     car.id = 'car'; 
     car.style.backgroundImage= ` url("/backgrnd/${i}.png")`;
