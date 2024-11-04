2a9a95aef509324ae775d9095389a24038d60c19
lazhar233 <lazhhar3@gmail.com>
2024-11-04 03:15:18 +0100

    seconde15

--- public/js/home.js
+++ public/js/home.js
@@ -1,23 +1,3 @@
-// const information =document.getElementById("informationHone");
-
-// information.innerHTML
-// document.getElementById("informationHone").innerHTML = `
-// ${userData[0].emil} :  ${userData[1].pas}
-// `;
-
-
-
-
-// import userData from '../../app';
-
-// console.log("User Data:", userData);
-// console.log("User Data:", userData);
-// console.log("User Data:", userData);
-// console.log("User Data:", userData);
-
-
-
-
 
 
 
@@ -35,6 +15,10 @@ bdy.addEventListener('click', function() {
 
 
 
+const  car= document.getElementById("car");
+bdy.addEventListener('click', function() {
+    window.location.href = "http://localhost:3000/home";
+});
 
 
 
@@ -42,37 +26,32 @@ bdy.addEventListener('click', function() {
 
 
 
+// for (let i = 1; i <= 6; i++) {
+//     care(i);
+// }
+
+// function care(i) {
+//     var car = document.createElement('div');
+//     var btn = document.createElement('button');
+//     var hsx = document.createElement('h6');
+//     var gren = document.createElement('div'); 
 
 
-for (let i = 1; i <= 4; i++) {
-    care(i);
-}
+//     btn.innerHTML = 'Click';  
+//     // const ray=[1,2,3,4]
+//     const ray = [{ num: "دهان" }, { num: "حلاف" }, { num: "بناء" }, { num: "سباك" }];
+//     hsx.innerHTML = `${ray[i-1].num}`;
 
-
-
-
-function care(i) {
-    var car = document.createElement('div');
-    var btn = document.createElement('button');
-    var hsx = document.createElement('h6');
-    var gren = document.createElement('div'); 
-
-
-    btn.innerHTML = 'Click';  
-    // const ray=[1,2,3,4]
-    const ray = [{ num: "دهان" }, { num: "حلاف" }, { num: "بناء" }, { num: "سباك" }];
-    hsx.innerHTML = `${ray[i-1].num}`;
-
-    car.id = 'car'; 
-    car.style.backgroundImage= ` url("/backgrnd/${i}.png")`;
+//     car.id = 'car'; 
+//     car.style.backgroundImage= ` url("/backgrnd/${i}.png")`;
     
-    gren.id = 'gren'; 
+//     gren.id = 'gren'; 
 
-    gren.appendChild(btn);
-    gren.appendChild(hsx);
-    car.appendChild(gren);
-    bdy.appendChild(car);
-}
+//     gren.appendChild(btn);
+//     gren.appendChild(hsx);
+//     car.appendChild(gren);
+//     bdy.appendChild(car);
+// }
       
 //  ---------------------------------------------------------------------------------------------------
 
